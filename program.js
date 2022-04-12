1bbebffce526294898580256659a4227d6d86e2e
Horizen23 <com100pb@gmail.com>
2022-04-13 03:30:13 +0700

    v2

--- program.js
+++ program.js
@@ -1,12 +1,13 @@
 function getuser(id){
     console.log({id,name:'anuchit'})
 }
-function getproduct(){
+
+function getproductII(){
     console.log([
-        {id:1,name:'เสื้อ'},
-        {id:2,name:'กางเกง'},
-        {id:3,name:'เกือก'}
+        {id:1,name:'เสื้อ',category:'เสื้อผ้า'},
+        {id:2,name:'กางเกง',category:'เสื้อผ้า'},
+        {id:3,name:'เมาส์',category:'เครืองใช้ไฟฟ้า'}
     ])
 }
 getuser(1)
-getproduct()
+getproductII()
