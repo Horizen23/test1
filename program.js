6a6d6973cc6cc59e2ac11e45bc1637cdfc30f1c0
Horizen23 <com100pb@gmail.com>
2022-04-13 03:25:58 +0700

    test1

---
+++ program.js
@@ -0,0 +1,12 @@
+function getuser(id){
+    console.log({id,name:'anuchit'})
+}
+function getproduct(){
+    console.log([
+        {id:1,name:'เสื้อ'},
+        {id:2,name:'กางเกง'},
+        {id:3,name:'เกือก'}
+    ])
+}
+getuser(1)
+getproduct()
